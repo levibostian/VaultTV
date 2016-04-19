0a7da73c8cd4eb1ac4fbc6eed2ccad6df070f46a
Levi Bostian <levi.bostian@gmail.com>
2016-04-18 21:00:30 -0500

    App opens slideshow full screen and refreshes every 30 minutes automatically.

--- app/src/main/java/com/levibostian/vaulttv/MainActivity.java
+++ app/src/main/java/com/levibostian/vaulttv/MainActivity.java
@@ -1,13 +1,49 @@
 package com.levibostian.vaulttv;
 
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.JsResult;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        getViews();
+
+        setupViews();
+    }
+
+    private void setupViews() {
+        mWebView.loadUrl("file:///android_asset/slideshow.html");
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.setWebChromeClient(new WebChromeClient() {
+
+            @Override
+            public boolean onJsAlert(WebView view, String url, String message, JsResult result) {
+                return super.onJsAlert(view, url, message, result);
+            }
+
+        });
+    }
+
+    public void getViews() {
+        mWebView = (WebView) findViewById(R.id.webview);
     }
 }
