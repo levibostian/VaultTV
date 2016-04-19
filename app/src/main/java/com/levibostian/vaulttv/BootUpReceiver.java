57fbd59ea413d904b9262a86fb8ff76f1361622b
Levi Bostian <levi.bostian@gmail.com>
2016-04-19 09:15:44 -0500

    Launch app when wifi connection is established.
    Better then on boot as on boot, it may not be connected to wifi yet.

--- app/src/main/java/com/levibostian/vaulttv/BootUpReceiver.java
+++ app/src/main/java/com/levibostian/vaulttv/BootUpReceiver.java
@@ -3,17 +3,25 @@ package com.levibostian.vaulttv;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 
 public class BootUpReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            Intent serviceIntent = new Intent(context, MainActivity.class);
-            serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-            context.startService(serviceIntent);
+        NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+        if (info != null && info.isConnected()) {
+            startApp(context);
         }
+    }
+
+    private void startApp(Context context) {
+        Intent serviceIntent = new Intent(context, MainActivity.class);
+        serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+        context.startActivity(serviceIntent);
     }
 
 }
